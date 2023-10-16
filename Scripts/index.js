b7840c742e52045fcf6a6e0871631874cdd255c3
Nevec02 <ignaciocarmonagonzalez@alumnos.tuniversformacion.es>
2023-10-16 21:38:25 +0200

    correciones

--- Scripts/index.js
+++ Scripts/index.js
@@ -33,6 +33,35 @@ document.addEventListener("DOMContentLoaded", () => {
     }
   });
 
+  btnTask.addEventListener("click", () => {
+    const taskText = taskInput.value.trim(); // Obtener el valor del input y eliminar espacios en blanco
+  
+    if (taskText === "") {
+      alert("Está vacío, introduce algo.");
+    } else {
+      const li = document.createElement("li");
+      const checkbox = document.createElement("input");
+      checkbox.type = "checkbox";
+      checkbox.className = "checkbox";
+  
+      const taskLabel = document.createElement("label");
+      taskLabel.textContent = taskText;
+  
+      checkbox.addEventListener("change", () => {
+        taskLabel.classList.toggle("completed", checkbox.checked);
+      });
+  
+      li.appendChild(checkbox);
+      li.appendChild(taskLabel);
+  
+      taskList.appendChild(li);
+  
+      taskInput.value = "";
+      taskInput.focus();
+    }
+  });
+  
+
   cleanList.addEventListener("click", () => {
     const task = document.querySelectorAll("li");
 
